de6f637073d75e85c6d7bf132a7f2d6eb3445873
Nacho <ignacio_coletta@hotmail.com>
2023-05-31 16:04:24 -0300

    deploy 31-05-23

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
