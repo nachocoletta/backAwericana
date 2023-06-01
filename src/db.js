6c33ac85ea06211a660fd3e411d7e7fd791622b2
Nacho <ignacio_coletta@hotmail.com>
2023-06-01 17:10:49 -0300

    deploy 01-06-23

--- src/db.js
+++ src/db.js
@@ -108,7 +108,7 @@ Favoritos.belongsTo(Publicacion);
 Favoritos.belongsTo(Usuario);
 
 
-
+// comentario de prueba para redeploy
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
