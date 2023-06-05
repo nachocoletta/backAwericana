e295ce975a147e31cfaac96d1b81f7594b534ad1
Nacho <ignacio_coletta@hotmail.com>
2023-06-05 00:44:45 -0300

    deploy 04/06/23

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const fakeRouter = require("../helpers/fakeGenerator");
+// const fakeRouter = require("../helpers/fakeGenerator");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/fake", fakeRouter);
+// router.use("/fake", fakeRouter);
 
 const productsRoute = require("./producto");
 const publicaciones = require("./publicaciones");
