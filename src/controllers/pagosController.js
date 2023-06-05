8cdee19632f049dbffe88e86b1323ca859b72f71
Nacho <ignacio_coletta@hotmail.com>
2023-06-05 10:31:29 -0300

    fix: deploy 05/06/23

--- src/controllers/pagosController.js
+++ src/controllers/pagosController.js
@@ -1,6 +1,6 @@
 const mercadopago = require("mercadopago");
 const { Publicacion, Carrito, Usuario, Direccion } = require("../db");
-const { finalizarPublicacion } = require("../helpers/finalizarPublicacion");
+const { finalizarPublicacion } = require("../Helpers/finalizarPublicacion");
 const { ACCESS_TOKEN_MP, URL_FRONT, URL,  URL_NOTIFICATION } = process.env;
 const { createPago } = require("../helpers/pagos.Helper");
 const calcularDistancia = require("../helpers/calcularEnvio");
