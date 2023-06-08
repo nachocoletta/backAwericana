6118bdf7ea387496a9bfde9e5b0b1343b2d0cfa3
Nacho <ignacio_coletta@hotmail.com>
2023-06-08 19:53:27 -0300

    fix: deploy 08-06-23

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://awericana.vercel.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", `${process.env.URL_FRONT}`); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
