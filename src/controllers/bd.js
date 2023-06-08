3509140230ea5db70ae378666a0fc65c7720412d
Nacho <ignacio_coletta@hotmail.com>
2023-06-08 16:27:50 -0300

    fix: deploy 08-06-23

--- src/controllers/bd.js
+++
@@ -1,8 +0,0 @@
-const poblarBaseDeDatos = require("../seeders/cargaBaseDeDatos");
-
-const iniciarBaseDeDatos = async (req, res) =>{
-    poblarBaseDeDatos();
-    res.json({msg: 'Se ha completado la carga en la base de datos.'});
-}
-
-module.exports = iniciarBaseDeDatos;
--- src/routes/imagenes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const { obtenerImagenes } = require("../controllers/imagen.js");
-
-const router = Router();
-
-router.get('/', obtenerImagenes)
-
-module.exports = router;
