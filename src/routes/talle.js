cbd5ebd6825d00f256623d23654667a4d838aae3
Nacho <ignacio_coletta@hotmail.com>
2023-06-09 12:19:51 -0300

    deploy 09-06-23

--- src/routes/talle.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const { obtenerTalles } = require("../controllers/talle.js");
-
-const router = Router();
-
-router.get('/', obtenerTalles)
-
-module.exports = router;
